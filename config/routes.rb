046645bd1b5d4ed2b80cf16429eedbd972f84fcd
Chris Goodwin <chris_2190@hotmail.com>
2019-08-27 12:17:54 +1000

    Created Show page - updated superhero controller, routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :superheroes, only: [:index, :new, :create]
+  resources :superheroes, only: [:index, :show, :new, :create]
   if Rails.env.development?
     get 'kitchensink', to: 'pages#kitchensink'
     get 'kitchensinkgael', to: 'pages#kitchensinkgael'
