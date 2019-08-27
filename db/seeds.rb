0b3ed654f651ad3aba94fc0f80ff283715c127cf
Michael Wang <michaelw0395@gmail.com>
2019-08-27 12:56:24 +1000

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Superhero.destroy_all
+puts 'Assembling superheroes...'
+
+superheroes_attributes = [
+  {
+    name:         'Batman',
+    price:        1000,
+    description:  'I am Batman.',
+    location:     'Gotham'
+  },
+  {
+    name:         'Superman',
+    price:        200,
+    description:  'I am Superman.',
+    location:     'Manhattan'
+  },
+  {
+    name:         'Ironman',
+    price:        3000,
+    description:  'I am Ironman.',
+    location:     'California'
+  },
+  {
+    name:         'Captain America',
+    price:        1,
+    description:  'I am Ironman.',
+    location:     'Canada'
+  },
+  {
+    name:         'Hulk',
+    price:        10000,
+    description:  'GRRRRRRrrrrrr',
+    location:     'Laboratory'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts "Finished creating #{restaurants_attributes.length} superheroes!"
