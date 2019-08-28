0319d12a9f5511a7dca5e0bed89a6bd24cd9e633
Chris Goodwin <chris_2190@hotmail.com>
2019-08-28 12:19:13 +1000

    Added User Controller and view. Linked Home page button to Host Landing Page.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
 
+  resources :users, only: [:index, :new, :create]
 
   if Rails.env.development?
     get 'kitchensink', to: 'pages#kitchensink'
